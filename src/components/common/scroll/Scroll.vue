6c617a686d57b6a1521eed2a1115fec3163d9d00
TangZhekun <1440529708@qq.com>
2022-03-20 22:22:05 +0800

    返回顶部 上拉加载

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -8,17 +8,47 @@
 import BScroll from 'better-scroll'
 export default {
   name: "Scroll",
+  props:{
+    probeType:Number,
+    default:0
+  },
+
   data(){
     return{
-      scroll: null
+      scroll: null,
+      pullUpLoad:{
+        type:Boolean,
+        default: false
+      }
     }
   },
   mounted() {
+    //1、创建scroll对象
     this.scroll = new BScroll(this.$refs.wrapper,{
       pullUpLoad: true,
       observeDOM: true,
-      click:true
+      click:true,
+      probeType: this.probeType
     })
+    //2、监听滚动的位置
+    this.scroll.on('scroll',position => {
+      this.$emit('scroll',position)
+    })
+    //监听上拉事件
+    this.scroll.on('pullingUp', () => {
+      this.$emit('pullingUp')//拉到页面底部之后将事件发送出去
+    })
+  },
+  methods:{
+    scrollTo(x,y,time=300){
+      this.scroll.scrollTo(x,y,time)
+    },
+    finishPullUp(){
+      this.scroll.finishPullUp()
+    },
+    refresh(){
+      this.scroll.refresh()
+    }
   }
 }
 </script>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,22 +3,20 @@
   <nav-bar class="home-nav">
     <template v-slot:center><div>购物街</div></template>
   </nav-bar>
-  <scroll class="content">
-    <ul>
-      <li>首页47</li>
-      <li>首页48</li>
-      <li>首页49</li>
-      <li>首页50</li>
-      <li>首页49</li>
-      <li>首页50</li>
-      <li>首页49</li>
-      <li>首页50</li>
-    </ul>
+  <scroll class="content"
+          ref="scroll"
+          :probe-type="3"
+          @scroll="contentScroll"
+          :pull-up-load="true"
+          @pullingUp="loadMore">
+    <popular-week></popular-week>
     <tab-control class="tab-control"
                  :title="['流行','新品','精选']"
                  @tabClick="tabClick"></tab-control>
     <goods-list :goods="goods[currentType].list"></goods-list>
   </scroll>
+<!--  组件不能直接监听点击事件 要加.native-->
+  <back-top @click.native="backClick" v-show="isShowBackTop"></back-top>
 </div>
 </template>
 
@@ -28,6 +26,8 @@ import TabControl from "@/components/context/tabControl/TabControl";
 import {getHomeMultidata,getHomeGoods} from "@/network/home";
 import GoodsList from "@/components/context/goods/GoodsList";
 import Scroll from "@/components/common/scroll/Scroll";
+import BackTop from "@/components/context/backTop/BackTop";
+import PopularWeek from "@/views/home/childComps/PopularWeek";
 
 export default {
   name: "Home",
@@ -35,7 +35,9 @@ export default {
     NavBar,
     TabControl,
     GoodsList,
-    Scroll
+    Scroll,
+    BackTop,
+    PopularWeek
   },
   created() {
     this.getHomeMultidata()
@@ -52,7 +54,8 @@ export default {
         'new':{page:0,list:[]},
         'sell':{page:0,list:[]}
       },
-      currentType: 'pop'
+      currentType: 'pop',
+      isShowBackTop: false
     }
   },
   methods:{
@@ -68,6 +71,7 @@ export default {
       getHomeGoods(type,page).then(res => {
         this.goods[type].list.push(...res.data.list)
         this.goods[type].page+=1
+        this.$refs.scroll.finishPullUp()
       })
     },
     tabClick(index){
@@ -82,6 +86,19 @@ export default {
           this.currentType='sell'
           break
       }
+    },
+    backClick(){
+      //给组件加上ref属性 通过this.$refs.name拿到组件对象
+      this.$refs.scroll.scrollTo(0,0) //传入x，y的坐标(想要回到的位置) 以及延迟时间
+    },
+    contentScroll(position){
+      this.isShowBackTop=position.y<-1000
+    },
+    //上拉加载更多
+    loadMore(){
+      this.getHomeGoods(this.currentType)
+      //调用refresh重新计算可滚动的区域
+      this.$refs.scroll.refresh()
     }
   }
 }
@@ -105,6 +122,8 @@ export default {
   top: 44px;
 }
 .content{
+
+
   overflow: hidden;
   position: absolute;
   top: 44px;
